acdb1ebccbd3bfd77559c264722762a9e0d0554e
LizDaCruz <87502205+LizDaCruz@users.noreply.github.com>
2024-08-01 11:41:34 -0400

    Create GuruService.go

---
+++ GuruService.go
@@ -0,0 +1,11 @@
+package guru
+
+import "context"
+
+// GuruService defines the methods for interacting with Guru
+type GuruService interface {
+    CreatePage(ctx context.Context, page Page) (string, error)
+    UpdatePage(ctx context.Context, page Page) error
+    DeletePage(ctx context.Context, id string) error
+    GetPage(ctx context.Context, id string) (Page, error)
+}
